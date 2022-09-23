4e4f2baff67dc3054dd4086ba0e39d2ffdba1340
Victor Rosager Christensen <cph-vc48@cphbusiness.dk>
2022-09-23 17:47:45 +0200

    added changes

--- get-script.js
+++ get-script.js
@@ -7,11 +7,68 @@ async function getData() {
 async function displayData(){
     const data = await getData();
 
-    const chartBox = document.getElementsByClassName("chartBox")[0];
-    console.log(chartBox)
+    console.log(data)
 
+    for(let i = 0; i < data.length ; i++) {
+
+    }
+
+    //const body = document.querySelector("body");
+    //let travelDestinationElement;
+}
+
+displayData();
+    /*
+
+    for(let i = 0; i < data.length ; i++) {
+        travelDestinationElement = document.createElement("div");
+        travelDestinationElement.classList = "travelDestination";
+        body.appendChild(travelDestinationElement);
+
+        let travelDestinationIdElement = document.createElement("p");
+        let travelDestinationId = document.createTextNode(`Id: ${data._id}`);
+        travelDestinationIdElement.appendChild(travelDestinationId);
+        travelDestinationElement.appendChild(travelDestinationIdElement);
+        */
+
+
+/*
+        let travelDestinationTitleElement = document.createElement("p");
+        let travelDestinationDescriptionElement = document.createElement("p");
+        let travelDestinationDateFromElement = document.createElement("p");
+        let travelDestinationDateToElement = document.createElement("p");
+        let travelDestinationCountryElement = document.createElement("p");
+        let travelDestinationLocationElement = document.createElement("p");
+        let travelDestinationGoogleMapsLinkElement = document.createElement("p");
+        */
+
+        /*
+        let travelDestinationTitle = document.createTextNode(`Title: ${data.title}`);
+        let travelDestinationDescription = document.createTextNode(`Description: ${data.description}`);
+        let travelDestinationDateFrom = document.createTextNode(`Date from: ${data.dateFrom}`);
+        let travelDestinationDateTo = document.createTextNode(`Date to: ${data.dateTo}`);
+        let travelDestinationCountry = document.createTextNode(`Country: ${data.country}`);
+        let travelDestinationLocation = document.createTextNode(`Location: ${data.location}`);
+        let travelDestinationGoogleMapsLink = document.createTextNode(`Google Maps link: ${data.googleMapsLink}`);
+        */
+
+        /*
+        travelDestinationTitleElement.appendChild(travelDestinationTitle);
+        travelDestinationDescriptionElement.appendChild(travelDestinationDescription);
+        travelDestinationDateFromElement.appendChild(travelDestinationDateFrom);
+        travelDestinationDateToElement.appendChild(travelDestinationDateTo);
+        travelDestinationCountryElement.appendChild(travelDestinationCountry);
+        travelDestinationLocationElement.appendChild(travelDestinationLocation);
+        travelDestinationGoogleMapsLinkElement.appendChild(travelDestinationGoogleMapsLink);
+        */
+/*
+    const body = document.querySelector("body");
+    body.appendChild(travelDestinationElement);
+
+    const travelDestination = document.querySelector("travelDestination")[0];
+    */
+    /*
     data.forEach((data, index) => {
-        let travelDestinationIdElement = document.createElement("div");
         let travelDestinationIdElement = document.createElement("p");
         let travelDestinationTitleElement = document.createElement("p");
         let travelDestinationDescriptionElement = document.createElement("p");
@@ -49,5 +106,4 @@ async function displayData(){
         chartBox.appendChild(travelDestinationGoogleMapsLinkElement);
     });
 }
-
-displayData();
+*/
